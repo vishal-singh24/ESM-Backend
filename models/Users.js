d01fea2075248fca5975ebb8e74931b9657fb6bf
Vishal Singh <vishalsingh@Vishals-MacBook-Air.local>
2025-03-31 12:26:17 +0530

    added separate login for employee and admin

--- models/Users.js
+++ models/Users.js
@@ -3,7 +3,7 @@ const bcrypt=require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true, required: true },
+  username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["admin", "employee"], required: true },
 });
