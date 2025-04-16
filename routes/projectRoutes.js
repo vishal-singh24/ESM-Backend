fd672337a43a7ef78c281d002b841ea0c8ee97ac
Vishal Singh <vishalsingh@Vishals-MacBook-Air.local>
2025-04-16 22:49:25 +0530

    added end-points for all projects and for way-points(few details are missing)

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,13 +2,23 @@ const express = require("express");
 const {
   createProject,
   assignEmployee,
-  getMyProjects
+  getMyProjects,
+  addWaypoint,
+  getProjectWaypoints,
+  updateWaypoint,
+  allProjects,
 } = require("../controllers/projectController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/create-project", authMiddleware("admin"), createProject);
 router.post("/:projectId/assign", authMiddleware("admin"), assignEmployee);
-router.get('/my-projects', authMiddleware('employee'), getMyProjects);
+router.get("/my-projects", authMiddleware("employee"), getMyProjects);
+router.get("/all-projects",authMiddleware(["admin"]),allProjects);
+
+router.post("/:projectId/waypoints", authMiddleware("employee"), addWaypoint);
+router.get("/:projectId/waypoints", authMiddleware("employee"), getProjectWaypoints);
+router.patch("/:projectId/waypoints/:waypointId", authMiddleware("employee"), updateWaypoint); 
+
 
 module.exports = router;
